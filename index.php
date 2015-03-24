3a3fceb5683714f8569b6e3943387af1331000fe
aichaDiak <aicha.diakite@outlook.com>
2015-03-24 17:17:25 +0100

    debut de mise en place de la structure des pages

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <div id="header"></div>
+        <div id="menu"></div>
+        <div id="main"></div>
+        <div id="footer"></div>
+    </body>
+</html>
